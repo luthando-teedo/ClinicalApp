094a4ac47fc0e8be83173dddca5b5d89bb59f7cc
luthando Sitole <luthando@quirky30.co.za>
2019-12-12 16:50:23 +0200

    add

---
+++ ClinicalPrism/MyMapControls/Controls/Maps/CustomMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace ControlExamples.Controls.Maps
+{
+    public class CustomMap : Map
+    {
+        public static readonly BindableProperty CenterRegionProperty = BindableProperty.Create("CenterRegion", typeof(MapSpan), typeof(CustomMap), null,propertyChanged: CenterRegionChanged);
+
+        private static void CenterRegionChanged(BindableObject sender, object oldValue, object newValue)
+        {
+            CustomMap map = (CustomMap)sender;
+
+            if (newValue is MapSpan location)
+            {
+                map.MoveToRegion(location);
+            }
+        }
+
+        public MapSpan CenterRegion
+        {
+            get { return (MapSpan)GetValue(CenterRegionProperty); }
+            set { SetValue(CenterRegionProperty, value); }
+        }
+
+
+    }
+}
---
+++ ClinicalPrism/MyMapControls/Controls/Maps/CustomPin.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace ControlExamples.Controls.Maps
+{
+    public class CustomPin : Pin
+    {
+        public static readonly BindableProperty NameProperty = BindableProperty.Create("Name", typeof(string), typeof(Pin), default(string));
+        public static readonly BindableProperty UrlProperty = BindableProperty.Create("Url", typeof(string), typeof(Pin), default(string));
+
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public string Url
+        {
+            get { return (string)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+    }
+}
