7ad79f3a69ee9e5980bbdea68365edaa8d9ba0e9
luthando Sitole <luthando@quirky30.co.za>
2019-12-12 17:15:34 +0200

    add

--- ClinicalPrism/Clinical/Clinical/Services/FakeSecurityService.cs
+++ ClinicalPrism/Clinical/Clinical/Services/FakeSecurityService.cs
@@ -100,7 +100,7 @@ namespace Clinical.Services
             menuItem = new MenuItem();
             menuItem.MenuItemId = 3;
             menuItem.MenuItemName = "Request Delivery";
-            menuItem.NavigationPath = "NavigationPage/RequestPage";
+            menuItem.NavigationPath = "NavigationPage/MapPage";
             menuItem.MenuOrder = 3;
             menuItem.MenuType = MenuTypeEnum.Secured;
             menuItem.ImageName = "location.png";
